69980ed93c1a2487b6e718b7ec5c83ee49db84bd
Shafiya24karam <shafiyakaram36@gmail.com>
2024-10-04 09:58:58 +0530

    Demo Code

---
+++ Demo1.java
@@ -0,0 +1,16 @@
+public class Demo1{
+	public static void main(String[] args){
+		String message="Good Night";
+		int grade=89;
+		double amount=88.12;
+	System.out.println(message);
+	System.out.println(grade);
+	System.out.println(amount);
+
+	}
+}
+declare 15 variables 
+reassign five variables 
+five bytes 
+five short 
+five int
